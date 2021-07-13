605289c410b0b002f01bde7a74deff972cad145f
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-07-13 04:16:49 +0100

    modified wepack configurations

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    entry: path.resolve(__dirname, '..', './client/index.tsx'),
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
@@ -37,7 +37,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', './src/index.html')
+            template: path.resolve(__dirname, '..', './client/index.html')
         })
     ]
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,12 +1,8 @@
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     devServer:{
         hot: true
     },
-    devtool: 'cheap-module-source-map',
-    plugins: [
-        new ReactRefreshWebpackPlugin()
-    ]
+    devtool: 'cheap-module-source-map'
 }
